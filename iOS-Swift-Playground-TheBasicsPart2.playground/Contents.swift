476afa66f2f9db8fe37057026a4781aeaab59929
Soonin Challenge <34075996+soonin@users.noreply.github.com>
2019-02-05 12:31:59 -0800

    Add files via upload

---
+++ iOS-Swift-Playground-TheBasicsPart2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// This is a comment.
+
+/* This is also a comment
+this is a test!
+this is a test!
+this is a test!
+ but is written over multiple lines. */
+
+let cat = "🐱"; print(cat);  print("this is a test!")
+// Prints "🐱"
+
+
