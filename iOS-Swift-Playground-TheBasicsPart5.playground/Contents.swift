bf5eb88b0661e37fc334c2398d2409e96f09e479
Soonin Challenge <34075996+soonin@users.noreply.github.com>
2019-02-05 13:30:40 -0800

    Add files via upload

---
+++ iOS-Swift-Playground-TheBasicsPart5.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let meaningOfLife = 42
+// meaningOfLife is inferred to be of type Int
+
+
+let meaningOfLife1 : Int8 = 42
+// meaningOfLife1 is inferred to be of type Int
+
+
+let pi = 3.14159
+// pi is inferred to be of type Double
+
+
+let anotherPi = 3 + 0.14159
+// anotherPi is also inferred to be of type Double
+
+
+let anotherPi1 : Float = 3 + 0.14159
+// anotherPi is also inferred to be of type Float
