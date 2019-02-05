e67ad8f22445b60c7da9d5ce8f20dea787235e6f
Soonin Challenge <34075996+soonin@users.noreply.github.com>
2019-02-05 13:20:01 -0800

    Add files via upload

---
+++ iOS-Swift-Playground-TheBasicsPart4.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let doubleValue = Double.infinity
+let NegativeDoubleValue = -doubleValue
+
+let floatValue = Float.infinity
+let NegativeFloatValue = -floatValue
+
+let floatValue1 = Float32.infinity
+let NegativeFloatValue1 = -floatValue1
+
+let numberValue2 = 3.1
+let NegativeNumberValue2 = -numberValue2
+
